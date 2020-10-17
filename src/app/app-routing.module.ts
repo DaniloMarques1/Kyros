d4ff1a773de7e207a44421eb8430c43dea20ff65
Danilo Marques <danilomarques20@hotmail.com>
2020-10-17 15:33:17 -0300

    Adding sign in button. Creating routing to home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SigninComponent } from './authenticate/signin/signin.component';
 import { SignupComponent } from './authenticate/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
@@ -15,6 +16,10 @@ const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
   }
 ];
 
